ed1cd1fd4619e9986d20546c8c0c9b324d35d576
Ambyr La Violette <alaviolette89@gmail.com>
2016-08-19 08:47:14 -0400

    inital upload

---
+++ src/testDataStructure/calculator.java
@@ -0,0 +1,98 @@
+package testDataStructure;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class calculator {
+	
+	public static void main(String[] arg){
+		
+		int last;
+		int first;
+		int math;
+	Stack<Integer> s = new Stack<Integer>();
+	
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Please input you equation");
+	String input= scan.nextLine(); 
+	//52*3+
+	//63-1+
+	
+	
+//	for(int i = 0 ; i<input.length() ;i++){
+//		char a = input.charAt(i);
+//	switch(a){
+//	case'+':
+//		last = s.pop();
+//		first = s.pop();
+//		 math = first + last;
+//		s.push(math);
+//		break;
+//	case'-':
+//		last = s.pop();
+//		first = s.pop();
+//		math = first - last;
+//		s.push(math);
+//		break;
+//		
+//	case'/':
+//		last = s.pop();
+//		first = s.pop();
+//		math = first / last;
+//		s.push(math);
+//		break;
+//		
+//	case'*':
+//		last = s.pop();
+//		first = s.pop();
+//		math = first * last;
+//		s.push(math);
+//		break;
+//	
+//	default:	
+//		int number = Character.getNumericValue(a);
+//		s.push(number);
+//		break;
+//		}
+//		}System.out.println(input+ " = "+s.pop());
+	
+	
+for(int i = 0 ; i<input.length() ;i++){
+	char a = input.charAt(i);
+	if( a >='0' && a<='9'){
+	int number = Character.getNumericValue(a);
+	s.push(number);
+	}else{
+		switch(a){
+		case'+':
+			last = s.pop();
+			first = s.pop();
+			 math = first + last;
+			s.push(math);
+			break;
+		case'-':
+			last = s.pop();
+			first = s.pop();
+			math = first - last;
+			s.push(math);
+			break;
+			
+		case'/':
+			last = s.pop();
+			first = s.pop();
+			math = first / last;
+			s.push(math);
+			break;
+		case'*':
+			last = s.pop();
+			first = s.pop();
+			math = first * last;
+			s.push(math);
+			break;
+		}
+	}
+	
+	}System.out.println(s.pop());
+	}
+}
